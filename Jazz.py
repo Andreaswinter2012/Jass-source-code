c7bbd98ca7e2e575bc41952f70284b2b3b4d6294
Andreaswinter2012 <andreaswinter1605@gmail.com>
2025-09-19 15:04:58 +0200

    Create Jazz.py

---
+++ Jazz.py
@@ -0,0 +1,30 @@
+command1 = 000
+#erste variable
+command2 = 000
+#zweite variable
+start = True
+while start:
+    try: 
+      punkte1 = int(input("punkte1:"))
+      #input1 variable
+      punkte2 = int(input("punkte2:"))
+      #input2 variable
+      command1 += punkte1
+      #dann wird erste variable addiert mit input variable
+      command2 += punkte2
+      #dann wird zweite variable addiert mit input variable
+    except:
+            print("Bitte screiben sie einen Zahl")
+         
+    if command1 == 1000:
+          print("Gruppe1 hat 1000p")
+          start = False
+    if command2 == 1000:
+          print("Gruppe2 hat 1000p")
+          start = False
+    if command1 > 1000:
+          print("Gruppe1 hat mehr als 1000p = "+(str(command1)))
+          start = False
+    if command2 > 1000:
+          print("Gruppe2 hat mehr als 1000p = "+(str(command2)))
+          start = False
