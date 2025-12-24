a46b6b37181196dc8233ed5bec2f9b16fddb51c1
Andreaswinter2012 <andreaswinter1605@gmail.com>
2025-12-24 11:48:19 +0100

    Add files via upload

---
+++ Jass-gui.py
@@ -0,0 +1,43 @@
+from pywebio.input import *
+from pywebio.output import put_text
+start = True
+while start:
+    command1 = 000
+    #erste variable
+    command2 = 000
+    #zweite variable
+    while start:
+        try: 
+            punkte1 = input("Team[1]:",type=NUMBER)
+            #input1 variable
+            command1 += punkte1
+            #dann wird erste variable addiert mit input variable
+            command2 += 157 - punkte1
+            #dann wird zweite variable addiert mit erste variable und minus 157
+            if command2 < 0:
+                command2 = 0
+            put_text("{")
+            put_text("Team[1]:"+str(command1))
+            put_text("Team[2]:"+str(command2))
+            put_text("}")
+            if command1 >= 1000:
+                put_text()
+                put_text("Team[1] hat 1000p oder mehr als 1000 = "+str(command1)+"🤩")
+                put_text()
+                start = False
+            if command2 >= 1000:
+                put_text()
+                put_text("Team[2] hat 1000p oder mehr als 1000 = "+str(command2)+"🤩")
+                put_text()
+                start = False
+
+        except:
+                put_text("Bitte screiben sie einen Zahl")
+
+    start = input('Screiben sie "exit" um zu beenden oder "next" um weiter spielen', type=TEXT)
+    if start == "exit":
+        start = False
+        put_text("Beenden")
+    if start == "next":
+        start = True
+        put_text("Weiter spielen")
